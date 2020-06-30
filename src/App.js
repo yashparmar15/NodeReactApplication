e4341d53b792fea20790e22cbd251a538480ebdb
Yash Parmar <yashparmar15700@gmail.com>
2020-06-30 16:32:03 +0530

    Project Repo and intro created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import AppBuilder from './containers/AppBuilder/AppBuilder';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppBuilder />
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const header = (props) => (
+    <div>
+        <h1>React Django Application</h1>
+        <h3>Contributors : Yash Parmar , Ayan Adhya and Viranch Patel (Students of IIT GOA).</h3>
+    </div>
+);
+
+export default header;
