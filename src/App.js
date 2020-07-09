e296b8e1b6f61f60e7e34d2d325957f00cbc3961
Ayan <ayan.adhya.18003@iitgoa.ac.in>
2020-07-09 17:31:32 +0530

    Project page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar';
 import AppBuilder from './containers/AppBuilder/AppBuilder';
+import Project from './containers/Project';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <AppBuilder />
+    <>
+      <Router>
+        <Navbar />
+        <Route exact path='/' component={AppBuilder} />
+        <Route exact path='/projects' component={Project} />
+      </Router>
+    </>
   );
 }
 
