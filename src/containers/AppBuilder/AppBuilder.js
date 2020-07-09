e163506ee7c37f490a9cda04a6e6819c8463c491
Yash Parmar <yashparmar15700@gmail.com>
2020-07-09 18:59:34 +0530

    Saved

--- src/containers/AppBuilder/AppBuilder.js
+++ src/containers/AppBuilder/AppBuilder.js
@@ -4,8 +4,11 @@ import Header from '../../components/Header/Header';
 
 import QuestionAnswer from '../../components/QuestionAnswer/QuestionAnswer';
 
+import UserProfilePage from '../../components/UserProfilePage/UserProfilePage';
+
 const appBuilder = (props) => (
-    <QuestionAnswer />
+    // <QuestionAnswer />
+    <UserProfilePage />
 );
 
 export default appBuilder;
