e9d8da4670f010be9f30342ef3ac8cfa9751c518
Ayan <ayan.adhya.18003@iitgoa.ac.in>
2020-07-09 21:22:46 +0530

    removed index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import Navbar from "./components/Navbar";
-import AppBuilder from "./containers/AppBuilder/AppBuilder";
-import Project from "./containers/Project";
-import TodoMain from "./containers/TodoMain";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import AppBuilder from './containers/AppBuilder/AppBuilder';
+import Project from './containers/Project';
+import TodoMain from './containers/TodoMain';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Navbar />
-				<Route exact path="/" component={AppBuilder} />
-				<Route exact path="/projects" component={Project} />
-				<Route exact path="/todo" component={TodoMain} />
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Route exact path='/' component={AppBuilder} />
+        <Route exact path='/projects' component={Project} />
+        <Route exact path='/todos' component={TodoMain} />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/todo/Doing.js
+++ src/components/todo/Doing.js
@@ -1,56 +1,47 @@
-import React from "react";
-import DoingItem from "./DoingItem";
-import Proptypes from "prop-types";
-import AddDoing from "./AddDoing";
+import React from 'react';
+import DoingItem from './DoingItem';
+import AddDoing from './AddDoing';
 
 class Doing extends React.Component {
-	state = {
-		addModalShow: false,
-	};
+  state = {
+    addModalShow: false,
+  };
 
-	onHide = () => {
-		this.setState({ addModalShow: false });
-	};
+  onHide = () => {
+    this.setState({ addModalShow: false });
+  };
 
-	render() {
-		return (
-			<div className="card border-warning mb-3 text-center">
-				<div className="card-header bg-warning font">Doing</div>
-				<div className="card-body text-warning">
-					<button
-						type="button"
-						className="btn btn-dark"
-						onClick={() => this.setState({ addModalShow: true })}
-					>
-						Add Task
-					</button>
-					<AddDoing
-						show={this.state.addModalShow}
-						onHide={this.onHide}
-						addDoing={this.props.addDoing}
-					/>
-					{this.props.doings.map((doing) => {
-						return (
-							<DoingItem
-								key={doing.id}
-								doing={doing}
-								laterdoing={this.props.laterdoing}
-								donetodo={this.props.donetodo}
-							/>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='card border-warning mb-3 text-center'>
+        <div className='card-header bg-warning font'>Doing</div>
+        <div className='card-body text-warning'>
+          <button
+            type='button'
+            className='btn btn-dark'
+            onClick={() => this.setState({ addModalShow: true })}
+          >
+            Add Task
+          </button>
+          <AddDoing
+            show={this.state.addModalShow}
+            onHide={this.onHide}
+            addDoing={this.props.addDoing}
+          />
+          {this.props.doings.map((doing) => {
+            return (
+              <DoingItem
+                key={doing.id}
+                doing={doing}
+                laterdoing={this.props.laterdoing}
+                donetodo={this.props.donetodo}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
-
-const style = {
-	width: "30%",
-};
-
-// Doing.propTypes = {
-// 	doings: Proptypes.array.isRequired,
-// };
 
 export default Doing;
