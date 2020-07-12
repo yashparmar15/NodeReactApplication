3598c1f14050996a6a224906b574bd5fb8041795
Viranch Patel <55953849+viranch044@users.noreply.github.com>
2020-07-12 13:07:56 +0530

    Update dev.js

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -6,6 +6,6 @@ module.exports = {
 	githubClientID: "81707b944c6a23b420b9",
 	githubClientSecret: "403649a29d6436d91d0501f516a628aa6b35e97c",
 	MONGO_URI:
-		"mongodb+srv://dbUser:dbUser@cluster0.x7c80.mongodb.net/<dbname>?retryWrites=true&w=majority",
+		"",
 	cookieKey: "eifninfeckinvifdslnlndsfvzdvnsnvisnfv",
 };
