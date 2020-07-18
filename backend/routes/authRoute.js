81bdfdafa15d978fe3d0ba7f2cde915e36f9f7d1
Viranch Patel <viranch.patel.18001@iitgoa.ac.in>
2020-07-19 00:44:35 +0530

    filter front end done

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,161 +1,184 @@
-const passport = require('passport');
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('../models/User');
+const passport = require("passport");
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/User");
 const router = express.Router();
-const Question = require('../models/Questions');
+const Question = require("../models/Questions");
 
 router.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"],
+	})
 );
 router.get(
-  '/auth/github',
-  passport.authenticate('github', {
-    scope: ['user: email'],
-  })
+	"/auth/github",
+	passport.authenticate("github", {
+		scope: ["user: email"],
+	})
 );
 
 router.get(
-  '/auth/google/callback',
-  passport.authenticate('google'),
-  (req, res) => {
-    if (req.user.flag) {
-      console.log(req.user);
-      const dummy = req.user;
-      dummy['flag'] = false;
-      User.findByIdAndUpdate(
-        req.user._id,
-        dummy,
-        { runValidators: true },
-        (err, response) => {
-          if (err) console.log(err);
-        }
-      );
-      res.redirect(`/info`);
-    } else res.redirect('/');
-  }
+	"/auth/google/callback",
+	passport.authenticate("google"),
+	(req, res) => {
+		if (req.user.flag) {
+			console.log(req.user);
+			const dummy = req.user;
+			dummy["flag"] = false;
+			User.findByIdAndUpdate(
+				req.user._id,
+				dummy,
+				{ runValidators: true },
+				(err, response) => {
+					if (err) console.log(err);
+				}
+			);
+			res.redirect(`/info`);
+		} else res.redirect("/");
+	}
 );
 router.get(
-  '/auth/github/callback',
-  passport.authenticate('github'),
-  (req, res) => {
-    if (req.user.flag) {
-      console.log(req.user);
-      const dummy = req.user;
-      dummy['flag'] = false;
-      User.findByIdAndUpdate(
-        req.user._id,
-        dummy,
-        { runValidators: true },
-        (err, response) => {
-          if (err) console.log(err);
-        }
-      );
-      res.redirect(`/info`);
-    } else res.redirect('/');
-  }
+	"/auth/github/callback",
+	passport.authenticate("github"),
+	(req, res) => {
+		if (req.user.flag) {
+			console.log(req.user);
+			const dummy = req.user;
+			dummy["flag"] = false;
+			User.findByIdAndUpdate(
+				req.user._id,
+				dummy,
+				{ runValidators: true },
+				(err, response) => {
+					if (err) console.log(err);
+				}
+			);
+			res.redirect(`/info`);
+		} else res.redirect("/");
+	}
 );
 
-router.get('/api/current_user', (req, res) => {
-  res.send(req.user);
+router.get("/api/current_user", (req, res) => {
+	res.send(req.user);
 });
 
-router.get('/api/logout', (req, res) => {
-  req.logout();
-  res.redirect(`/`);
+router.get("/api/logout", (req, res) => {
+	req.logout();
+	res.redirect(`/`);
 });
 
-router.post('/api/questions', async (req, res, next) => {
-  const content = await req.body.content;
-  const question = await req.body.question;
-  const tags = await req.body.allTags;
-  const askedBy = await req.user;
-  // console.log(req.body);
-  // console.log('Question:', question);
-  // console.log('DraftJS:', content);
-  // console.log('Tags:', tags);
-  if (question) {
-    new Question({
-      title: question,
-      description: content,
-      askedBy: askedBy,
-      tags: tags,
-    })
-      .save()
-      .then(() => {
-        console.log('Saved!');
-      })
-      .catch((err) => console.log(err));
-  }
+router.post("/api/questions", async (req, res, next) => {
+	const content = await req.body.content;
+	const question = await req.body.question;
+	const tags = await req.body.allTags;
+	const askedBy = await req.user;
+	const filtertags = await req.body.filtertags;
 
-  res.redirect('/questions');
-  //
+	// console.log(req.body);
+	// console.log("Question:", question);
+	// console.log("DraftJS:", content);
+	// console.log("Tags:", tags);
+	// if (question === "" && filtertags.length !== 0) {
+	// 	console.log(filtertags.length);
+	// 	console.log("filterwala h ");
+	// }
+	if (question) {
+		new Question({
+			title: question,
+			description: content,
+			askedBy: askedBy,
+			tags: tags,
+		})
+			.save()
+			.then(() => {
+				console.log("Saved!");
+			})
+			.catch((err) => console.log(err));
+	}
+
+	res.redirect("/questions");
+	//
 });
 
-router.post('/api/questions/:id', async (req, res, next) => {
-  const answer = await req.body.answer;
-  console.log(answer);
-  const id = req.params.id;
-  // console.log('Answer:', answer);
-  // console.log(id);
-  // console.log(req.user.username);
+router.post("/api/questions/:id", async (req, res, next) => {
+	const answer = await req.body.answer;
+	console.log(answer);
+	const id = req.params.id;
+	// console.log('Answer:', answer);
+	// console.log(id);
+	// console.log(req.user.username);
 
-  if (answer) {
-    const currAns = {
-      answer: answer,
-      askedBy: req.user._id,
-      username: req.user.username,
-    };
-    console.log(currAns);
-    await Question.findById(id)
-      .exec()
-      .then((doc) => {
-        if (doc) {
-          doc.answers.push(currAns);
-          doc
-            .save()
-            .then((savedDoc) => {
-              return res.status(200).json({
-                error: {},
-              });
-            })
-            .catch((err) => {
-              return res.status(500);
-            });
-        } else {
-          return res.status(404);
-        }
-      })
-      .catch((err) => {
-        return res.status(500);
-      });
-  }
+	if (answer) {
+		const currAns = {
+			answer: answer,
+			askedBy: req.user._id,
+			username: req.user.username,
+		};
+		console.log(currAns);
+		await Question.findById(id)
+			.exec()
+			.then((doc) => {
+				if (doc) {
+					doc.answers.push(currAns);
+					doc
+						.save()
+						.then((savedDoc) => {
+							return res.status(200).json({
+								error: {},
+							});
+						})
+						.catch((err) => {
+							return res.status(500);
+						});
+				} else {
+					return res.status(404);
+				}
+			})
+			.catch((err) => {
+				return res.status(500);
+			});
+	}
 
-  res.redirect('/questions');
-  //
+	res.redirect("/questions");
+	//
 });
 
-router.get('/api/questions', async (req, res) => {
-  await Question.find()
-    .populate('askedBy')
-    .then((questions) => {
-      // console.log(questions)
-      res.send(questions);
-    })
-    .catch((err) => console.log(err));
+router.get("/api/questions", async (req, res) => {
+	await Question.find()
+		.populate("askedBy")
+		.then((questions) => {
+			// console.log(questions)
+			res.send(questions);
+		})
+		.catch((err) => console.log(err));
 });
 
-router.get('/api/logout', async (req, res) => {
-  await req.logout();
-  req.session = null;
-  res.clearCookie('ayan', { path: '/', httpOnly: true });
-  res.clearCookie('ayan.sig', { path: '/', httpOnly: true });
-  // res.clearCookie('session', { path: '/', httpOnly: true });
-  // res.clearCookie('session.sig', { path: '/', httpOnly: true });
-  res.redirect('/');
+router.post("/api/filter", async (req, res, next) => {
+	const tags = await req.body.allTags;
+	console.log(req.body);
+	console.log("Tags:", tags);
+	console.log(req.body);
+
+	// console.log(req.body);
+	console.log(4);
+	await Question.find()
+		.populate("askedBy")
+		.then((questions) => {
+			// console.log(questions)
+			res.send(questions);
+		})
+		.catch((err) => console.log(err));
+});
+
+router.get("/api/logout", async (req, res) => {
+	await req.logout();
+	req.session = null;
+	res.clearCookie("ayan", { path: "/", httpOnly: true });
+	res.clearCookie("ayan.sig", { path: "/", httpOnly: true });
+	// res.clearCookie('session', { path: '/', httpOnly: true });
+	// res.clearCookie('session.sig', { path: '/', httpOnly: true });
+	res.redirect("/");
 });
 
 // function isUserAuthenticated(req, res, next) {
--- src/components/QuestionAnswer/Part2/Part2.js
+++ src/components/QuestionAnswer/Part2/Part2.js
@@ -1,29 +1,143 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './Part2.css';
+import "./Part2.css";
 
-import Questions from './Questions/Questions';
+import Questions from "./Questions/Questions";
+import makeAnimated from "react-select/animated";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import 'font-awesome/css/font-awesome.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "font-awesome/css/font-awesome.min.css";
+import axios from "axios";
+import Select from "react-select";
+const querystring = require("query-string");
+// import { all } from "../../../../backend/routes/authRoute";
 
-const part2 = (props) => (
-    <div className = "part2">
-        <div className = "head">
-            <h1 className = "heading">All Questions</h1>
-            <div className = "filter">
-                 Filter By <select className = "options"><option>Select Categories</option></select>
-            </div>
-        </div>
-        <div className = "filter2">
-            <h3 className="filt">Latest</h3>
-            <h3 className="filt">Votes</h3>
-            <h3 className="filt">Unanswered</h3>
-            <div className = "perpage">Questions Per Page: <select className = "ques"><option>12</option></select></div>
-        </div>
-        <Questions />
-    </div>
-);
+const animatedComponents = makeAnimated();
+
+class part2 extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tags: [
+				{ value: "Python", label: "Python" },
+				{ value: "Java", label: "Java" },
+				{ value: "C++", label: "C++" },
+				{ value: "ReactJS", label: "ReactJS" },
+				{ value: "NodeJs", label: "NodeJs" },
+			],
+			Submit: "Submit",
+		};
+
+		this.customTheme = (theme) => {
+			return {
+				...theme,
+				colors: {
+					...theme.colors,
+					primary25: "orange",
+					primary: "green",
+				},
+			};
+		};
+		let allTags = [];
+		this.handleTagChange = (selectedTag) => {
+			allTags = [];
+			this.setState({ selectedTag });
+			if (selectedTag) {
+				selectedTag.map((o) => {
+					allTags.push(o.value);
+					console.log(allTags);
+					return allTags;
+				});
+			}
+		};
+
+		this.saveContent = (allTags) => {
+			console.log(allTags);
+			console.log(4);
+			const filtertags = allTags;
+			const question = "";
+			const content = "";
+			axios
+				.post("/api/questions", {
+					filtertags,
+					question,
+					content,
+					allTags,
+				})
+				.then((res) => {
+					console.log(res);
+				});
+		};
+
+		this.onSubmit = (e) => {
+			e.preventDefault();
+			console.log(allTags);
+			// const contentState = editorState.getCurrentContent();
+			this.setState({
+				Submit: "Submiited!",
+				className_submit: "btn btn-success btn-md",
+			});
+
+			setTimeout(() => {
+				this.setState({
+					Submit: "Submit",
+					className_submit: "btn btn-primary btn-md",
+				});
+			}, 2000);
+
+			this.saveContent(allTags);
+		};
+	}
+	render() {
+		return (
+			<div className="part2">
+				<div className="head">
+					<h1 className="heading">All Questions</h1>
+					<form method="post" onSubmit={this.onSubmit}>
+						<div className="filter">
+							Filter By{" "}
+							{/* <select className="options">
+							<option>Select Categories</option>
+						</select> */}
+							<Select
+								closeMenuOnSelect={false}
+								components={animatedComponents}
+								//   defaultValue={options}
+								isMulti
+								options={this.state.tags}
+								placeholder="Tags.."
+								theme={this.customTheme}
+								// onChange={setName}
+								onChange={this.handleTagChange}
+								noOptionsMessage={() => "Tag not found 😞 "}
+								className="mb-3"
+							/>
+							<button
+								type="submit"
+								// onClick={this.props.close}
+								className={this.state.className_submit}
+							>
+								{this.state.Submit}{" "}
+							</button>
+						</div>
+					</form>
+				</div>
+				<div className="filter2">
+					<h3 className="filt">Latest</h3>
+					<h3 className="filt">Votes</h3>
+					<h3 className="filt">Unanswered</h3>
+					<div className="perpage">
+						Questions Per Page:{" "}
+						<select className="ques">
+							<option>12</option>
+						</select>
+					</div>
+				</div>
+				<Questions />
+			</div>
+		);
+	}
+}
 
 export default part2;
