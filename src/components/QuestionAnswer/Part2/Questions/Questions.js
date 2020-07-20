0c1bb8a38034f239ea3347f748df9f3216ee2f6b
Viranch Patel <viranch.patel.18001@iitgoa.ac.in>
2020-07-20 15:27:21 +0530

    Top Question and ANswers k users wala done

--- src/components/QuestionAnswer/Part2/Questions/Questions.js
+++ src/components/QuestionAnswer/Part2/Questions/Questions.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import Question from './Question/Question';
-import Loader from '../../../../assets/loaders/svg-loaders/oval.svg';
-import './Questions.css';
+import Question from "./Question/Question";
+import Loader from "../../../../assets/loaders/svg-loaders/oval.svg";
+import "./Questions.css";
 
 function Questions(props) {
-  return (
-    <>
-      {props.questions.questionLoading && (
-        <img src={Loader} className='questionLoader' alt='loader' />
-      )}
-      <div className=''>
-        {props.questions.questions.map((Q) => (
-          <span key={Q._id}>
-            {' '}
-            <Question
-              title={Q.title}
-              description={Q.description}
-              answers={Q.answers}
-              id={Q._id}
-              totalanswers={Q.answers.length}
-              askedBy={Q.askedBy}
-              date={Q.date}
-              allTags={Q.tags}
-              upvotes={Q.upvotes}
-              downvotes={Q.downvotes}
-            />{' '}
-          </span>
-        ))}
-      </div>
-    </>
-  );
+	return (
+		<>
+			{props.questions.questionLoading && (
+				<img src={Loader} className="questionLoader" alt="loader" />
+			)}
+			<div className="">
+				{props.questions.questions.map((Q) => (
+					<span key={Q._id}>
+						{" "}
+						<Question
+							title={Q.title}
+							description={Q.description}
+							answers={Q.answers}
+							id={Q._id}
+							totalanswers={Q.answers.length}
+							askedBy={Q.askedBy}
+							date={Q.date}
+							allTags={Q.tags}
+							upvotes={Q.upvotes}
+							downvotes={Q.downvotes}
+						/>{" "}
+					</span>
+				))}
+			</div>
+		</>
+	);
 }
 
 const mapStateToProps = (state) => {
-  return {
-    questions: state.question,
-  };
+	return {
+		questions: state.question,
+	};
 };
 
 export default connect(mapStateToProps)(Questions);
